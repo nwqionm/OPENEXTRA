3dccd0bd01fcd7ce67ff93c51b5c8cc92c21c010
nwqionm <35755067+nwqionm@users.noreply.github.com>
2018-01-30 21:40:32 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -75,8 +75,8 @@ echo -e "|${RED}4${NC}| WEB PANEL ${RED} ✖   ${NC}"
 echo -e "|${RED}5${NC}| VNSTAT (CHECK BANDWIDTH or DATA) ${RED} ✖   ${NC}"
 echo -e "|${RED}6${NC}| SQUID PROXY ${GREEN} ✔   ${NC}"
 echo "	Ubuntu 14.04 - 16.04 - 17.04"
-echo "	Debian 8 - 9"
-echo -e "|${RED}7${NC}| VNSTAT MONITOR IN WEBSITE ${RED} ✖   ${NC}"
+echo "	Debian 7- 8 - 9"
+# echo -e "|${RED}7${NC}| VNSTAT MONITOR IN WEBSITE ${RED} ✖   ${NC}"
 echo -e "|${RED}8${NC}| REMOVE SQUID PROXY ${GREEN} ✔   ${NC}"
 echo ""
 echo -e "${RED}ฟังก์ชั่นที่ 1 และ 2 เลือกอย่างใดอย่างหนึ่งเท่านั้น${NC}"
@@ -351,248 +351,6 @@ END
 		if [[ -e /etc/squid3/squid.conf ]]; then
 			apt-get -y remove --purge squid3
 		fi
-	apt-get -y install squid3
-	cat > /etc/squid3/squid.conf <<END
-http_port $PROXY
-acl localhost src 127.0.0.1/32 ::1
-acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
-acl localnet src 10.0.0.0/8
-acl localnet src 172.16.0.0/12
-acl localnet src 192.168.0.0/16
-acl SSL_ports port 443
-acl Safe_ports port 80
-acl Safe_ports port 21
-acl Safe_ports port 443
-acl Safe_ports port 70
-acl Safe_ports port 210
-acl Safe_ports port 1025-65535
-acl Safe_ports port 280
-acl Safe_ports port 488
-acl Safe_ports port 591
-acl Safe_ports port 777
-acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
-http_access allow SSH
-http_access allow localnet
-http_access allow localhost
-http_access deny all
-refresh_pattern ^ftp:           1440    20%     10080
-refresh_pattern ^gopher:        1440    0%      1440
-refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
-refresh_pattern .               0       20%     4320
-END
-			IP2="s/xxxxxxxxx/$IP/g";
-			sed -i $IP2 /etc/squid3/squid.conf;
-			service squid3 restart
-
-	elif [[ "$VERSION_ID" = 'VERSION_ID="9"' || "$VERSION_ID" = 'VERSION_ID="16.04"' || "$VERSION_ID" = 'VERSION_ID="17.04"' ]]; then
-		if [[ -e /etc/squid/squid.conf ]]; then
-			apt-get -y remove --purge squid
-		fi
-	apt-get -y install squid
-	cat > /etc/squid/squid.conf <<END
-http_port $PROXY
-acl localhost src 127.0.0.1/32 ::1
-acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
-acl localnet src 10.0.0.0/8
-acl localnet src 172.16.0.0/12
-acl localnet src 192.168.0.0/16
-acl SSL_ports port 443
-acl Safe_ports port 80
-acl Safe_ports port 21
-acl Safe_ports port 443
-acl Safe_ports port 70
-acl Safe_ports port 210
-acl Safe_ports port 1025-65535
-acl Safe_ports port 280
-acl Safe_ports port 488
-acl Safe_ports port 591
-acl Safe_ports port 777
-acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
-http_access allow SSH
-http_access allow localnet
-http_access allow localhost
-http_access deny all
-refresh_pattern ^ftp:           1440    20%     10080
-refresh_pattern ^gopher:        1440    0%      1440
-refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
-refresh_pattern .               0       20%     4320
-END
-			IP2="s/xxxxxxxxx/$IP/g";
-			sed -i $IP2 /etc/squid/squid.conf;
-			service squid restart
-	fi
-
-	wget -O /usr/local/bin/menu "https://raw.githubusercontent.com/nwqionm/OPENEXTRA/master/menu"
-	chmod +x /usr/local/bin/menu
-	newclient "$CLIENT"
-	cp /root/$CLIENT.ovpn /home/vps/public_html/
-	rm -f /root/$CLIENT.ovpn
-	useradd -e `date -d "365 days" +"%Y-%m-%d"` -s /bin/false -M $CLIENT
-	EXP="$(chage -l $CLIENT | grep "Account expires" | awk -F": " '{print $2}')"
-	echo -e "$CLIENT\n$CLIENT\n"|passwd $CLIENT &> /dev/null
-	echo ""
-	echo "Source by Mnm Ami"
-	echo "Donate via TrueMoney Wallet : 082-038-2600"
-	echo ""
-	echo "OpenVPN, Squid Proxy, Nginx .....Install finish."
-	echo "IP server : $IP"
-	echo "Port : $PORT"
-	echo "Protocal : TCP"
-	echo "Proxy : $IP"
-	echo "Port proxy : $PROXY"
-	echo "Download config (only you) : $IP:85/$CLIENT.ovpn"
-	echo ""
-	echo "====================================================="
-	echo "ติดตั้งสำเร็จ... กรุณาพิมพ์คำสั่ง menu เพื่อไปยังขั้นตอนถัดไป"
-	echo "====================================================="
-	exit
-fi
-
-	;;
-
-	2)
-
-IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-if [[ "$IP" = "" ]]; then
-		IP=$(wget -4qO- "http://whatismyip.akamai.com/")
-fi
-
-if [[ "$VERSION_ID" != 'VERSION_ID="8"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="14.04"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="16.04"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="17.04"' ]]; then
-	echo ""
-	echo "เวอร์ชั่น OS ของคุณเป็นเวอร์ชั่นเก่าที่ไม่รองรับแล้ว"
-	echo "สำหรับเวอร์ชั่นที่รองรับได้ จะมีดังนี้..."
-	echo ""
-	echo "Ubuntu 14.04 - 16.04 - 17.04"
-	echo "Debian 8 - 9"
-	echo ""
-	exit
-else
-	echo ""
-	echo "OS ที่คุณใช้ไม่สามารถรองรับได้กับสคริปท์นี้"
-	echo "สำหรับ OS ที่รองรับได้ จะมีดังนี้..."
-	echo ""
-	echo "Ubuntu 14.04 - 16.04 - 17.04"
-	echo "Debian 8 - 9"
-	echo ""
-	exit
-fi
-
-	# Debian 8
-	if [[ "$VERSION_ID" = 'VERSION_ID="8"' ]]; then
-
-		echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" > /etc/apt/sources.list.d/mongodb-org-3.6.list
-		echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-		apt-get update
-		apt-get --assume-yes install pritunl mongodb-org
-		systemctl start mongod pritunl
-		systemctl enable mongod pritunl
-
-			while [[ $Squid3 != "Y" && $Squid3 != "N" ]]; do
-
-				echo ""
-				read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid3
-
-			done
-
-	# Debian 9
-	elif [[ "$VERSION_ID" = 'VERSION_ID="9"' ]]; then
-
-		echo "deb http://repo.pritunl.com/stable/apt stretch main" > /etc/apt/sources.list.d/pritunl.list
-		apt-get -y install dirmngr
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-		apt-get update
-		apt-get --assume-yes install pritunl mongodb-server
-		systemctl start mongodb pritunl
-		systemctl enable mongodb pritunl
-
-			while [[ $Squid != "Y" && $Squid != "N" ]]; do
-
-				echo ""
-				read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
-
-			done
-
-	# Ubuntu 14.04
-	elif [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
-
-		echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
-		echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-		apt-get update
-		apt-get --assume-yes install pritunl mongodb-org
-		service pritunl start
-
-			while [[ $Squid3 != "Y" && $Squid3 != "N" ]]; do
-
-				echo ""
-				read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
-
-			done
-
-	# Ubuntu 16.04
-	elif [[ "$VERSION_ID" = 'VERSION_ID="16.04"' ]]; then
-
-		echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
-		echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-		apt-get update
-		apt-get --assume-yes install pritunl mongodb-org
-		systemctl start pritunl mongod
-		systemctl enable pritunl mongod
-
-			while [[ $Squid != "Y" && $Squid != "N" ]]; do
-
-				echo ""
-				read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
-
-			done
-
-	# Ubuntu 17.04
-	elif [[ "$VERSION_ID" = 'VERSION_ID="17.04"' ]]; then
-
-		echo "deb http://repo.pritunl.com/stable/apt zesty main" > /etc/apt/sources.list.d/pritunl.list
-		apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-		apt-get update
-		apt-get --assume-yes install pritunl mongodb-server
-		systemctl start pritunl mongodb
-		systemctl enable pritunl mongodb
-
-			while [[ $Squid != "Y" && $Squid != "N" ]]; do
-
-				echo ""
-				read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
-
-			done
-
-	fi
-
-	if [[ "$Squid3" = "N" || "$Squid" = "N" ]]; then
-
-		echo ""
-		echo "Source by Mnm Ami"
-		echo "Donate via TrueMoney Wallet : 082-038-2600"
-		echo ""
-		echo "Pritunl .....Install Finish."
-		echo "No Proxy"
-		echo ""
-		echo "Pritunl : http://$IP"
-		echo ""
-		pritunl setup-key
-		echo ""
-		exit
-
-	fi
-
-	if [[ "$Squid3" = "Y" ]]; then
-
-		echo ""
-		read -p "Port proxy : " -e -i 8080 PROXY
 
 		apt-get -y install squid3
 		cat > /etc/squid3/squid.conf <<END
@@ -627,24 +385,11 @@ END
 		IP2="s/xxxxxxxxx/$IP/g";
 		sed -i $IP2 /etc/squid3/squid.conf;
 		service squid3 restart
-		echo ""
-		echo "Source by Mnm Ami"
-		echo "Donate via TrueMoney Wallet : 082-038-2600"
-		echo ""
-		echo "Pritunl .....Install Finish."
-		echo "Proxy : $IP"
-		echo "Port  : $PROXY"
-		echo ""
-		echo "Pritunl : http://$IP"
-		echo ""
-		pritunl setup-key
-		echo ""
-		exit
 
-	elif [[ "$Squid" = "Y" ]]; then
-		
-		echo ""
-		read -p "Port proxy : " -e -i 8080 PROXY
+	elif [[ "$VERSION_ID" = 'VERSION_ID="9"' || "$VERSION_ID" = 'VERSION_ID="16.04"' || "$VERSION_ID" = 'VERSION_ID="17.04"' ]]; then
+		if [[ -e /etc/squid/squid.conf ]]; then
+			apt-get -y remove --purge squid
+		fi
 
 		apt-get -y install squid
 		cat > /etc/squid/squid.conf <<END
@@ -679,20 +424,261 @@ END
 		IP2="s/xxxxxxxxx/$IP/g";
 		sed -i $IP2 /etc/squid/squid.conf;
 		service squid restart
-		echo ""
-		echo "Source by Mnm Ami"
-		echo "Donate via TrueMoney Wallet : 082-038-2600"
-		echo ""
-		echo "Pritunl .....Install Finish."
-		echo "Proxy : $IP"
-		echo "Port  : $PROXY"
-		echo ""
-		echo "Pritunl : http://$IP"
-		echo ""
-		pritunl setup-key
-		echo ""
-		exit
 	fi
+
+	wget -O /usr/local/bin/menu "https://raw.githubusercontent.com/nwqionm/OPENEXTRA/master/menu"
+	chmod +x /usr/local/bin/menu
+	newclient "$CLIENT"
+	cp /root/$CLIENT.ovpn /home/vps/public_html/
+	rm -f /root/$CLIENT.ovpn
+	useradd -e `date -d "365 days" +"%Y-%m-%d"` -s /bin/false -M $CLIENT
+	EXP="$(chage -l $CLIENT | grep "Account expires" | awk -F": " '{print $2}')"
+	echo -e "$CLIENT\n$CLIENT\n"|passwd $CLIENT &> /dev/null
+	echo ""
+	echo "Source by Mnm Ami"
+	echo "Donate via TrueMoney Wallet : 082-038-2600"
+	echo ""
+	echo "OpenVPN, Squid Proxy, Nginx .....Install finish."
+	echo "IP server : $IP"
+	echo "Port : $PORT"
+	echo "Protocal : TCP"
+	echo "Proxy : $IP"
+	echo "Port proxy : $PROXY"
+	echo "Download config (only you) : $IP:85/$CLIENT.ovpn"
+	echo ""
+	echo "====================================================="
+	echo "ติดตั้งสำเร็จ... กรุณาพิมพ์คำสั่ง menu เพื่อไปยังขั้นตอนถัดไป"
+	echo "====================================================="
+	exit
+
+fi
+
+	;;
+
+	2)
+
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+if [[ "$IP" = "" ]]; then
+		IP=$(wget -4qO- "http://whatismyip.akamai.com/")
+fi
+
+if [[ "$VERSION_ID" != 'VERSION_ID="8"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="9"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="14.04"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="16.04"' ]] && [[ "$VERSION_ID" != 'VERSION_ID="17.04"' ]]; then
+	echo ""
+	echo "เวอร์ชั่น OS ของคุณเป็นเวอร์ชั่นเก่าที่ไม่รองรับแล้ว"
+	echo "สำหรับเวอร์ชั่นที่รองรับได้ จะมีดังนี้..."
+	echo ""
+	echo "Ubuntu 14.04 - 16.04 - 17.04"
+	echo "Debian 8 - 9"
+	echo ""
+	exit
+else
+	echo ""
+	echo "OS ที่คุณใช้ไม่สามารถรองรับได้กับสคริปท์นี้"
+	echo "สำหรับ OS ที่รองรับได้ จะมีดังนี้..."
+	echo ""
+	echo "Ubuntu 14.04 - 16.04 - 17.04"
+	echo "Debian 8 - 9"
+	echo ""
+	exit
+fi
+
+# Debian 8
+if [[ "$VERSION_ID" = 'VERSION_ID="8"' ]]; then
+	echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" > /etc/apt/sources.list.d/mongodb-org-3.6.list
+	echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	apt-get update
+	apt-get --assume-yes install pritunl mongodb-org
+	systemctl start mongod pritunl
+	systemctl enable mongod pritunl
+
+		while [[ $Squid3 != "Y" && $Squid3 != "N" ]]; do
+			echo ""
+			read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid3
+		done
+
+# Debian 9
+elif [[ "$VERSION_ID" = 'VERSION_ID="9"' ]]; then
+	echo "deb http://repo.pritunl.com/stable/apt stretch main" > /etc/apt/sources.list.d/pritunl.list
+	apt-get -y install dirmngr
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	apt-get update
+	apt-get --assume-yes install pritunl mongodb-server
+	systemctl start mongodb pritunl
+	systemctl enable mongodb pritunl
+
+		while [[ $Squid != "Y" && $Squid != "N" ]]; do
+			echo ""
+			read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
+		done
+
+# Ubuntu 14.04
+elif [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
+	echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
+	echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	apt-get update
+	apt-get --assume-yes install pritunl mongodb-org
+	service pritunl start
+
+		while [[ $Squid3 != "Y" && $Squid3 != "N" ]]; do
+			echo ""
+			read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
+		done
+
+# Ubuntu 16.04
+elif [[ "$VERSION_ID" = 'VERSION_ID="16.04"' ]]; then
+	echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
+	echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	apt-get update
+	apt-get --assume-yes install pritunl mongodb-org
+	systemctl start pritunl mongod
+	systemctl enable pritunl mongod
+
+		while [[ $Squid != "Y" && $Squid != "N" ]]; do
+			echo ""
+			read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
+		done
+
+# Ubuntu 17.04
+elif [[ "$VERSION_ID" = 'VERSION_ID="17.04"' ]]; then
+	echo "deb http://repo.pritunl.com/stable/apt zesty main" > /etc/apt/sources.list.d/pritunl.list
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	apt-get update
+	apt-get --assume-yes install pritunl mongodb-server
+	systemctl start pritunl mongodb
+	systemctl enable pritunl mongodb
+
+		while [[ $Squid != "Y" && $Squid != "N" ]]; do
+			echo ""
+			read -p "ต้องการติดตั้ง Squid Proxy หรือไม่ (Y or N) : " -e -i Y Squid
+		done
+
+fi
+
+if [[ "$Squid3" = "N" || "$Squid" = "N" ]]; then
+	echo ""
+	echo "Source by Mnm Ami"
+	echo "Donate via TrueMoney Wallet : 082-038-2600"
+	echo ""
+	echo "Pritunl .....Install Finish."
+	echo "No Proxy"
+	echo ""
+	echo "Pritunl : http://$IP"
+	echo ""
+	pritunl setup-key
+	echo ""
+	exit
+
+fi
+
+if [[ "$Squid3" = "Y" ]]; then
+	echo ""
+	read -p "Port proxy : " -e -i 8080 PROXY
+
+	apt-get -y install squid3
+	cat > /etc/squid3/squid.conf <<END
+http_port $PROXY
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl localnet src 10.0.0.0/8
+acl localnet src 172.16.0.0/12
+acl localnet src 192.168.0.0/16
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+http_access allow SSH
+http_access allow localnet
+http_access allow localhost
+http_access deny all
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+END
+	IP2="s/xxxxxxxxx/$IP/g";
+	sed -i $IP2 /etc/squid3/squid.conf;
+	service squid3 restart
+	echo ""
+	echo "Source by Mnm Ami"
+	echo "Donate via TrueMoney Wallet : 082-038-2600"
+	echo ""
+	echo "Pritunl .....Install Finish."
+	echo "Proxy : $IP"
+	echo "Port  : $PROXY"
+	echo ""
+	echo "Pritunl : http://$IP"
+	echo ""
+	pritunl setup-key
+	echo ""
+	exit
+
+elif [[ "$Squid" = "Y" ]]; then
+	echo ""
+	read -p "Port proxy : " -e -i 8080 PROXY
+
+	apt-get -y install squid
+	cat > /etc/squid/squid.conf <<END
+http_port $PROXY
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl localnet src 10.0.0.0/8
+acl localnet src 172.16.0.0/12
+acl localnet src 192.168.0.0/16
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+http_access allow SSH
+http_access allow localnet
+http_access allow localhost
+http_access deny all
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+END
+	IP2="s/xxxxxxxxx/$IP/g";
+	sed -i $IP2 /etc/squid/squid.conf;
+	service squid restart
+	echo ""
+	echo "Source by Mnm Ami"
+	echo "Donate via TrueMoney Wallet : 082-038-2600"
+	echo ""
+	echo "Pritunl .....Install Finish."
+	echo "Proxy : $IP"
+	echo "Port  : $PROXY"
+	echo ""
+	echo "Pritunl : http://$IP"
+	echo ""
+	pritunl setup-key
+	echo ""
+	exit
+
+fi
 
 	;;
 
@@ -735,6 +721,7 @@ if [[ "$VERSION_ID" = 'VERSION_ID="7"' || "$VERSION_ID" = 'VERSION_ID="8"' || "$
 	if [[ -e /etc/squid3/squid.conf ]]; then
 		apt-get -y remove --purge squid3
 	fi
+
 	apt-get -y install squid3
 	cat > /etc/squid3/squid.conf <<END
 http_port $PROXY
@@ -782,6 +769,7 @@ elif [[ "$VERSION_ID" = 'VERSION_ID="9"' || "$VERSION_ID" = 'VERSION_ID="16.04"'
 	if [[ -e /etc/squid/squid.conf ]]; then
 		apt-get -y remove --purge squid
 	fi
+
 	apt-get -y install squid
 	cat > /etc/squid/squid.conf <<END
 http_port $PROXY
@@ -834,24 +822,27 @@ fi
 
 	8)
 
-	if [[ -e /etc/squid/squid.conf ]]; then
-		apt-get -y remove --purge squid
-		echo ""
-		echo "Squid proxy removed."
-		echo ""
-		exit
-	elif [[ -e /etc/squid3/squid.conf ]]; then
-		apt-get -y remove --purge squid3
-		echo ""
-		echo "Squid proxy removed."
-		echo ""
-		exit
-	else
-		echo ""
-		echo "คุณยังไม่เคยติดตั้ง Squid Proxy"
-		echo ""
-		exit
-	fi
+if [[ -e /etc/squid/squid.conf ]]; then
+	apt-get -y remove --purge squid
+	echo ""
+	echo "Squid proxy removed."
+	echo ""
+	exit
+
+elif [[ -e /etc/squid3/squid.conf ]]; then
+	apt-get -y remove --purge squid3
+	echo ""
+	echo "Squid proxy removed."
+	echo ""
+	exit
+
+else
+	echo ""
+	echo "คุณยังไม่เคยติดตั้ง Squid Proxy"
+	echo ""
+	exit
+
+fi
 
 	;;
 
