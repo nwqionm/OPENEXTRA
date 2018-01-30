8a1321bce3cc06751792c3cf7e6091f860b190d2
nwqionm <35755067+nwqionm@users.noreply.github.com>
2018-01-31 06:30:44 +0700

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-cat > /var/www/html/vnstat/config.php <<END
-<?php
+mkdir /var/www/html/vnstat /var/www/html/vnstat/css
+
+echo "<?php
+
 error_reporting(E_ALL | E_NOTICE);
 $vnstat_config_format_hour = "H";
 $vnstat_bin_dir = '/usr/local/bin/vnstat';
 $use_predefined_interfaces = true;
 $byte_formatter = "MB";
-if ($use_predefined_interfaces == true) {
-$interface_list = array("eth0");
-$interface_name['eth0'] = "Internal";
-} else {
-$interface_list = get_vnstat_interfaces($vnstat_bin_dir);
 
-foreach ($interface_list as $interface)
-{
-$interface_name[$interface] = $interface;
+if ($use_predefined_interfaces == true) {
+    $interface_list = array("eth0");
+
+    $interface_name['eth0'] = "Internal";
+} else {
+    $interface_list = get_vnstat_interfaces($vnstat_bin_dir);
+    
+    foreach ($interface_list as $interface)
+    {
+        $interface_name[$interface] = $interface;
+    }
 }
-}
-?>
-END
+?>" > /var/www/html/vnstat/config.php
