e0b9503605cb23504e9af4a854ff2b0bbbd50277
nwqionm <35755067+nwqionm@users.noreply.github.com>
2018-02-18 17:48:14 +0700

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,98 +1,21 @@
 #!/bin/bash
 
-if [[ $USER != 'root' ]]; then
-	echo "Maaf, Anda harus menjalankan ini sebagai root"
-	exit
+ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+if [[ $ether = "" ]]; then
+	ether=eth0
+elif [[ $ether = "" ]]; then
+	ether=tun0
+elif [[ $ether = "" ]]; then
+	ether=ens18
 fi
 
-# go to root
-cd
-
-u="user1"
-p="pass1"
-
-# get the VPS IP
-#ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
-ip=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
-if [ "$ip" = "" ]; then
-	ip=$(wget -qO- ipv4.icanhazip.com)
+if [[ $ether = "eth0" ]]; then
+echo "eth0"
+exit
+elif [[ $ether = "tun0" ]]; then
+echo "tun0"
+exit
+elif [[ $ether = "ens18" ]]; then
+echo "ens18"
+exit
 fi
-
-echo
-echo "######################################################"
-echo "Downloading and Installing PoPToP"
-echo "######################################################"
-apt-get update
-apt-get -y install pptpd
-
-echo
-echo "######################################################"
-echo "Creating Server Config"
-echo "######################################################"
-cat > /etc/ppp/pptpd-options <<END
-name pptpd
-refuse-pap
-refuse-chap
-refuse-mschap
-require-mschap-v2
-require-mppe-128
-ms-dns 208.67.222.222
-ms-dns 208.67.220.220
-proxyarp
-nodefaultroute
-lock
-nobsdcomp
-END
-
-# setting up pptpd.conf
-echo "option /etc/ppp/pptpd-options" > /etc/pptpd.conf
-echo "logwtmp" >> /etc/pptpd.conf
-echo "localip $ip" >> /etc/pptpd.conf
-echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
-
-# adding new user
-echo "$u	*	$p	*" >> /etc/ppp/chap-secrets
-
-echo
-echo "######################################################"
-echo "Forwarding IPv4 and Enabling it on boot"
-echo "######################################################"
-cat >> /etc/sysctl.conf <<END
-net.ipv4.ip_forward=1
-END
-sysctl -p
-
-echo
-echo "######################################################"
-echo "Updating IPtables Routing and Enabling it on boot"
-echo "######################################################"
-iptables -t nat -A POSTROUTING -j SNAT --to $ip
-# saves iptables routing rules and enables them on-boot
-iptables-save > /etc/iptables.conf
-
-cat > /etc/network/if-pre-up.d/iptables <<END
-#!/bin/sh
-iptables-restore < /etc/iptables.conf
-END
-
-chmod +x /etc/network/if-pre-up.d/iptables
-cat >> /etc/ppp/ip-up <<END
-ifconfig ppp0 mtu 1400
-END
-
-echo
-echo "######################################################"
-echo "Restarting PoPToP"
-echo "######################################################"
-sleep 5
-/etc/init.d/pptpd restart
-
-echo
-echo "######################################################"
-echo "Server setup complete!"
-echo "Connect to your VPS at $ip with these credentials:"
-echo "Username:$u ##### Password: $p"
-echo "######################################################"
-
-cd ~/
-rm -f /root/IP
